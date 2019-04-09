8ed27676f64552496a544b1daf81b2f85af33371
Margherita Guido <margherita.guido@mail.polimi.it>
2019-04-09 09:16:02 +0000

    code

--- C_main2D.m
+++ C_main2D.m
@@ -1,4 +1,4 @@
-function [errors,solutions,femregion,Dati]=C_main2D(TestName,nRef)
+function [errors,solutions,femregion,Dati,Peclet]=C_main2D(TestName,nRef,Stab)
 %==========================================================================
 % Solution of the Poisson's problem with linear finite elements
 % (non homogeneous Dirichlet boundary conditions)
@@ -14,11 +14,11 @@ function [errors,solutions,femregion,Dati]=C_main2D(TestName,nRef)
 %                        solution
 %          femregion   : (struct) infos about finite elements
 %                        discretization
-%          Matrices    : (struct) fe stiffness and mass matrices
 %          Dati        : (struct)  see C_dati.m
+%          Peclet      : (vector) local Peclet
 %          
 % Usage: 
-%    [errors,solutions,femregion,Matrices,Dati] = C_main2D('Test1',3)
+%    [errors,solutions,femregion,Dati] = C_main2D('Test1',3)
  
 
 
@@ -34,7 +34,7 @@ addpath Postprocessing
 % LOAD DATA FOR TEST CASE
 %==========================================================================
 
-Dati = C_dati(TestName);
+Dati = C_dati(TestName,Stab);
 Dati.nRefinement = nRef;
 
 %==========================================================================
@@ -53,88 +53,38 @@ Dati.nRefinement = nRef;
 % BUILD FINITE ELEMENT MATRICES and RIGHT-HAND SIDE
 %==========================================================================
 
-%add the Build of the mass matrix
-[M_no_bc,A_no_bc,b_no_bc] = C_matrix2D(Dati,femregion);
- %M and A does not depend on time 
- 
- %time integration: we need to know the initial time( suppose it eq to 0) 
- %final time 
- 
- t = 0;
- T = Dati.T;
- dt = Dati.dt;
- theta = Dati.theta;
- 
- %initial condition evaluated in x and y (nodal points)
- x = femregion.coord(:,1);
- y = femregion.coord(:,2);
- 
- %evaluation of initial condition 
- u0 = eval(Dati.initialcond);
-  
- %I plot the initial solution
- C_snapshot_sol(femregion, u0, Dati,t)
- 
- %lhs matrix
- K_no_bc = M_no_bc+theta*dt*A_no_bc;
- 
- %evaluation of force wrt time 
- f0 = eval(Dati.forcetime);
- 
- for t = dt : dt : T 
-    %evaluation of force wrt time in t(k+1)
-    f1 = eval(Dati.forcetime);
- 
-    %rhs
-    f_no_bc= (M_no_bc - (1 - theta)*dt*A_no_bc)*u0...
-          + dt * (theta*b_no_bc *f1 + (1 - theta)*b_no_bc*f0);
-      
-     
-    %======================================================================
-    % COMPUTE BOUNDARY CONDITIONS -- MODIFICATION OF A an b
-    %======================================================================
+[A_no_bc,b_no_bc,Peclet] = C_matrix2D(Dati,femregion);
 
-    [K,f,u_g] = C_bound_cond2D(K_no_bc,f_no_bc,femregion,Dati,t);
+%==========================================================================
+% COMPUTE BOUNDARY CONDITIONS -- MODIFICATION OF A an b
+%==========================================================================
 
-    %======================================================================
-    % SOLVE THE LINEAR SYSTEM
-    %======================================================================
-    %you can also do the factorization of the matrix outside the time loop 
-    %and use it at each step to solve the linear system (the matrix does
-    %not depend on time)
-    
-    u1 = K\f;
+[A,b,u_g] = C_bound_cond2D(A_no_bc,b_no_bc,femregion,Dati);
 
-    %======================================================================
-    % ASSIGN DIRICHLET BOUNDARY CONDITIONS -- through the lifting ug
-    %======================================================================
+%==========================================================================
+% SOLVE THE LINEAR SYSTEM
+%==========================================================================
 
-    u1 = u1 + u_g;
-    
-    %plot the current solution
-    C_snapshot_sol(femregion, u1, Dati,t)
-    
-    %update for the next step
-    f0 = f1;
-    u0 = u1;
-    
- %end of time integration 
- end
+uh = A\b;
+
+%==========================================================================
+% ASSIGN DIRICHLET BOUNDARY CONDITIONS -- through the lifting ug
+%==========================================================================
+
+uh = uh + u_g;
+
 %==========================================================================
 % POST-PROCESSING OF THE SOLUTION
 %==========================================================================
 
-[solutions] = C_postprocessing(Dati,femregion,u1,t);
+[solutions] = C_postprocessing(Dati,femregion,uh);
 
 %==========================================================================
 % ERROR ANALYSIS
 %==========================================================================
 errors = [];
-
-%we compute the error wrt the space for a fixed istant 
-
 if (Dati.plot_errors)
-    [errors] = C_compute_errors(Dati,femregion,solutions,t);
+    [errors] = C_compute_errors(Dati,femregion,solutions);
 end
 
 
