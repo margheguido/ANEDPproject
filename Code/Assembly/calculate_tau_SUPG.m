f03b0ab5cccdaee1ef83a4cc3ca830e8e5dd4e99
Margherita Guido <margherita.guido@mail.polimi.it>
2019-08-17 16:30:54 +0000

    script to generate database

--- Code/Assembly/calculate_tau_SUPG.m
+++ Code/Assembly/calculate_tau_SUPG.m
@@ -1,6 +1,8 @@
 function [ tau ] = calculate_tau_SUPG( femregion, pphys_2D, Dati ,Peclet)
 
 delta = coth(Peclet)-1/Peclet;
+delta2=delta*1e12;
+delta3=delta*1e-2;
 
 x = pphys_2D( :, 1 );
 y = pphys_2D( :, 2 );
@@ -9,7 +11,7 @@ b2_phys = eval( Dati.b2 );
 b_phys = [ b1_phys, b2_phys ];    % i = nodo, j = direzione
 b_phys_mean = mean( b_phys, 1 );
 
-tau = delta * femregion.h / norm( b_phys_mean );
+tau = delta* femregion.h / norm( b_phys_mean );
 
 end
 
