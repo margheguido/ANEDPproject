0a72801391f1b66b696fb3507eb05825d566e09c
Michele Vidulis <michele.vidulis@mail.polimi.it>
2019-08-18 09:44:53 +0000

    Added test 3, the one used by GuidoVidulisADRExactSol

--- Code/check_best_tau.m
+++ Code/check_best_tau.m
@@ -3,13 +3,14 @@ close all
 
 %% Test1
 
-mu = 1e-5;
-nRef = 3;
+test = 'Test3';
+mu = 1e-9;
+nRef = 5;
 
 ii = 1;
-stabilization_amount = 10.^[ -2, -1, -0.5, -0.2, 0, 0.2, 0.5, 1, 2 ]; %( -2 : 3 );
+stabilization_amount = 10.^[ -5, -4, -3, -2, -1, -0.5, -0.2, 0, 0.2, 0.5, 1, 2 ]; %( -2 : 3 );
 for stab = stabilization_amount
-    [ errors, solutions, femregion, Dati, Peclet, tau ] = C_main2D( 'Test1', nRef, stab, mu );
+    [ errors, solutions, femregion, Dati, Peclet, tau ] = C_main2D( test, nRef, stab, mu );
     err_L2( ii ) = errors.Error_L2;
     err_H1( ii ) = errors.Error_H1;
     ii = ii + 1;
