a2b2d3f27635af4901f84b3b93c7eb9440fb795b
Michele Vidulis <michele.vidulis@mail.polimi.it>
2019-08-27 13:33:11 +0000

    Dataset generation for comparison on Gauss points

--- Code/generate_training_solutions_from_exact.m
+++ Code/generate_training_solutions_from_exact.m
@@ -3,15 +3,25 @@ close all
 
 SUPG_ANN_dataset = fopen( 'SUPG.txt', 'w' );
 
+compute_on_gauss_points = 1; % 0 -> sample on dofs, 1 -> sample on n_gauss_points gauss points per element
+
 % Print headers
 fprintf( SUPG_ANN_dataset, 'mu          \t' );
-n_dofs = 25;         % n_dofs = 9 --> nRef = 1, n_dofs = 25 --> nRef = 2, n_dofs = 81 --> nRef = 3, ...
-for i = 1 : n_dofs
-    fprintf( SUPG_ANN_dataset, 'dof_%1.0f        \t', i );
+n_dofs = 81;         % n_dofs = 9 ---> nRef = 1, n_dofs = 25 ---> nRef = 2, n_dofs = 81 ---> nRef = 3, ...
+n_elems = 64;        % n_elems = 4 --> nRef = 1, n_elems = 16 --> nRef = 2, n_elems = 64 --> nRef = 3, ...
+n_gauss_points = 4;  % gauss points to integrate over a songle element
+for i = 1 : n_elems
+    for k = 1 : n_gauss_points
+        fprintf( SUPG_ANN_dataset, 'e_%1.0f_p_%1.0f       \t', i, k );
+    end
 end
 fprintf( SUPG_ANN_dataset, '\n' );
 
-for mu = 1 : -1e-1 : 0.1 % 0.5 * 1e-5 : 1e-6 : 1.5 * 1e-5 
+for mu = linspace( 1e-7, 1e-3, 50 )
+%          [ 1e-6, 2e-6, 4e-6, 8e-6,...
+%            1e-5, 2e-5, 3e-5, 4e-5, 6e-5, 8e-5,...
+%            1e-4, 2e-4, 3e-4, 4e-4, 5e-4, 6e-4, 7e-4, 8e-4, 9e-4,...
+%            1e-3 ] 
     
 %     [errors,solutions,femregion,Dati,Peclet,tau] = C_main2D('Test1',1,0,mu);
 %     exact_solution = Dati.exact_sol;
@@ -21,16 +31,51 @@ for mu = 1 : -1e-1 : 0.1 % 0.5 * 1e-5 : 1e-6 : 1.5 * 1e-5
 %     exact_solution = '-atan(((x-0.5).^2+(y-0.5).^2-1/16)./(sqrt(mu)))';  % Test2, tipo panettone
     exact_solution = 'sin(2*pi*x).*(y-y.^2)';                            % Test3, due gobbe (GuidoVidulisADRExactSol)
     
-    % Compute sampling points (mesh vertexes)
+    % Compute sampling points
     h = 1 / ( sqrt( n_dofs ) - 1 );
     x = [];
-    for i = 1 : sqrt( n_dofs )
-       x = [ x, 0 : h : 1 ]; 
-    end
     y = [];
-    for i = 0 : sqrt( n_dofs ) - 1
-       y = [ y, ones( 1, sqrt( n_dofs ) ) * h * i ]; 
+    
+    if compute_on_gauss_points == 0
+        
+        for i = 1 : sqrt( n_dofs )
+           x = [ x, 0 : h : 1 ]; 
+        end
+        for i = 0 : sqrt( n_dofs ) - 1
+           y = [ y, ones( 1, sqrt( n_dofs ) ) * h * i ]; 
+        end
+        
+    elseif compute_on_gauss_points == 1
+        
+        val_1 = 0.098584391824351;
+        val_2 = 0.026415608175648;
+        
+        current_element = 1;
+        
+        for Y = 0 : h : h*( sqrt( n_dofs ) - 2 )
+           
+            for X = 0 : h : h*( sqrt( n_dofs ) - 2 )
+               
+                x( ( current_element - 1 ) * 4 + 1 ) = X + val_1;
+                y( ( current_element - 1 ) * 4 + 1 ) = Y + val_1;
+                
+                x( ( current_element - 1 ) * 4 + 2 ) = X + val_2;
+                y( ( current_element - 1 ) * 4 + 2 ) = Y + val_1;
+                
+                x( ( current_element - 1 ) * 4 + 3 ) = X + val_1;
+                y( ( current_element - 1 ) * 4 + 3 ) = Y + val_2;
+                
+                x( ( current_element - 1 ) * 4 + 4 ) = X + val_2;
+                y( ( current_element - 1 ) * 4 + 4 ) = Y + val_2;
+               
+                current_element = current_element + 1;
+                
+            end
+           
+        end
+       
     end
+        
     
     % Evaluate exact solution at sampling points
     sampled_exact_solution = eval( exact_solution );
